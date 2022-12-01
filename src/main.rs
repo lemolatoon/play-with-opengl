14f64247550f6f7eb5947057110555d6e6051f78
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2022-12-01 17:31:24 +0900

    Add window display

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use std::time::Duration;
+
 fn main() {
     println!("Hello, world!");
+
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem
+        .window("SDL", 640, 480)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+    canvas.set_draw_color(Color::RGB(255, 255, 255));
+    canvas.clear();
+    canvas.present();
+    loop {}
 }
